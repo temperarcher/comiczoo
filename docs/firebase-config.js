b92c0758da78a594c278b4c6ec34d5ecd9892193
temperarcher <temper.archer@gmail.com>
2025-12-25 19:49:39 +0100

    Create firebase-config.js

---
+++ docs/firebase-config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCMWWtqeymGjCxQiQbGcIEZUAEkhaUt-TI",
+  authDomain: "comiczoo.firebaseapp.com",
+  projectId: "comiczoo",
+  storageBucket: "comiczoo.firebasestorage.app",
+  messagingSenderId: "854173216358",
+  appId: "1:854173216358:web:3b0485ceab341046f907a7"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const auth = firebase.auth();
+
