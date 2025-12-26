b3bc0a7bf3bada17bf7fef0458da2ba8e0df637b
temperarcher <temper.archer@gmail.com>
2025-12-26 18:54:48 +0100

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -1,2 +1,10 @@
 console.log("App JS caricato correttamente");
 
+// Test Firebase
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    console.log("Utente autenticato: ", user);
+  } else {
+    console.log("Nessun utente autenticato.");
+  }
+});
