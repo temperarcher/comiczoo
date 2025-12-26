03a7069a99dc080c23e0f69cecfa2b9b843a1395
temperarcher <temper.archer@gmail.com>
2025-12-26 18:52:44 +0100

    Update firebase-config.js

--- docs/firebase-config.js
+++ docs/firebase-config.js
@@ -4,6 +4,8 @@
 //  storageBucket: "comiczoo.firebasestorage.app",
 //  messagingSenderId: "854173216358",
 //  appId: "1:854173216358:web:3b0485ceab341046f907a7"
+// firebase-config.js
+
 var firebaseConfig = {
   apiKey: "AIzaSyCMWWtqeymGjCxQiQbGcIEZUAEkhaUt-TI",
   authDomain: "comiczoo.firebaseapp.com",
