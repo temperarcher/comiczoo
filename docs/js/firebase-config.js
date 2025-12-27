84726cdf066d39d30678da3136e61771e7a7fe23
temperarcher <temper.archer@gmail.com>
2025-12-27 18:33:53 +0100

    Update firebase-config.js

--- docs/js/firebase-config.js
+++ docs/js/firebase-config.js
@@ -6,11 +6,12 @@
 //  appId: "1:854173216358:web:3b0485ceab341046f907a7"
 // firebase-config.js
 
-// Firebase SDK v9 (modulare)
+// Importa i moduli di Firebase necessari
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
+// Configurazione di Firebase (prendi i dati dal tuo progetto Firebase)
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCMWWtqeymGjCxQiQbGcIEZUAEkhaUt-TI",
@@ -22,6 +23,11 @@ const firebaseConfig = {
 };
 
 
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+// Inizializza Firebase
+const app = initializeApp(firebaseConfig);
+
+// Inizializza Firestore e Auth
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { auth, db, GoogleAuthProvider, signInWithPopup, signOut };
