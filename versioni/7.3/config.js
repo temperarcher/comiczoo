abb81b85aa96d9911afa4fa12a0f918275269f00
temperarcher <temper.archer@gmail.com>
2026-01-15 21:54:34 +0100

    Delete config.js

--- versioni/7.3/config.js
+++
@@ -1,9 +0,0 @@
-// config.js
-const SUPABASE_URL = 'https://ljylddlredinveheaagd.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_KkV7wImbPxzJfQkroc_ciA_HkbZiAKB';
-
-// Inizializza il client
-const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
-
-// Esportiamo una funzione o l'oggetto per usarlo altrove
-window.supabaseClient = _supabase;
