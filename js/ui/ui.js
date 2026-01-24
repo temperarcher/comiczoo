1d538525a685ebfdfa55978cbb82f4fcbc6cedd6
temperarcher <temper.archer@gmail.com>
2026-01-24 23:12:34 +0100

    Create ui.js

---
+++ js/ui/ui.js
@@ -0,0 +1,27 @@
+/**
+ * VERSION: 1.0.0  
+ */
+import { header } from './ui/layout/header.js';
+import { publishers } from './ui/layout/publishers.js';
+import { series } from './ui/layout/series.js';
+import { container } from './ui/grid/container.js';
+import { card } from './ui/grid/card.js';
+import { base } from './ui/modal/base.js';
+import { stories } from './ui/modal/stories.js';
+import { form } from './ui/modal/form.js';
+
+export const UI = {
+    HEADER: header,
+    PUBLISHER_SECTION: publishers.SECTION,
+    PUBLISHER_PILL: publishers.PILL,
+    ALL_PUBLISHERS_BUTTON: publishers.ALL_BUTTON,
+    SERIE_SECTION: series.SECTION,
+    SERIE_ITEM: series.ITEM,
+    MAIN_GRID_CONTAINER: container,
+    ISSUE_CARD: card,
+    MODAL_WRAPPER: base.WRAPPER,
+    MODAL_LAYOUT: base.LAYOUT,
+    ISSUE_FORM: form,
+    STORY_ROW: stories.ROW,
+    CHARACTER_TAG: stories.CHARACTER
+};
