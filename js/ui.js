9b7faf28cfac6afdda0f9197597587c7d99cfb7a
temperarcher <temper.archer@gmail.com>
2026-01-25 18:29:59 +0100

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,16 +1,18 @@
 /**
- * VERSION: 1.1.3
+ * VERSION: 1.1.4
  * PROTOCOLLO DI INTEGRITÀ: È FATTO DIVIETO DI OTTIMIZZARE O SEMPLIFICARE PARTI CONSOLIDATE.
- * IN CASO DI MODIFICHE NON INTERESSATE DAL TASK, COPIARE E INCOLLARE INTEGRALMENTE IL CODICE PRECEDENTE.
  */
 import { sections } from './ui/layout/sections.js';
 import { header } from './ui/layout/header.js';
 import { publishers } from './ui/layout/publishers.js';
+import { series } from './ui/layout/series.js'; // Import aggiunto
 
 export const UI = {
     ROOTS: sections,
     HEADER: header,
     PUBLISHER_SECTION: publishers.SECTION,
     PUBLISHER_PILL: publishers.PILL,
-    ALL_PUBLISHERS_BUTTON: publishers.ALL_BUTTON
+    ALL_PUBLISHERS_BUTTON: publishers.ALL_BUTTON,
+    SERIES_SECTION: series.SECTION, // Componenti serie aggiunti all'HUB
+    SERIES_CARD: series.CARD
 };
