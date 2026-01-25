fd6a1027f10357c0996172ca612fcf4334d9641e
temperarcher <temper.archer@gmail.com>
2026-01-25 11:54:21 +0100

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,12 +1,16 @@
 /**
- * VERSION: 1.0.0
+ * VERSION: 1.1.0
  * PROTOCOLLO DI INTEGRITÀ: È FATTO DIVIETO DI OTTIMIZZARE O SEMPLIFICARE PARTI CONSOLIDATE.
  * IN CASO DI MODIFICHE NON INTERESSATE DAL TASK, COPIARE E INCOLLARE INTEGRALMENTE IL CODICE PRECEDENTE.
  */
 import { sections } from './ui/layout/sections.js';
 import { header } from './ui/layout/header.js';
+import { publishers } from './ui/layout/publishers.js'; // Import aggiunto
 
 export const UI = {
     ROOTS: sections,
-    HEADER: header
+    HEADER: header,
+    PUBLISHER_SECTION: publishers.SECTION, // Componenti aggiunti all'HUB
+    PUBLISHER_PILL: publishers.PILL,
+    ALL_PUBLISHERS_BUTTON: publishers.ALL_BUTTON
 };
