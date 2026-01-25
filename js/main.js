3106e1f7014a6d40db218667bc7273fa43a0204e
temperarcher <temper.archer@gmail.com>
2026-01-25 12:09:25 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 /**
- * VERSION: 1.0.0
+ * VERSION: 1.1.1
  * PROTOCOLLO DI INTEGRITÀ: È FATTO DIVIETO DI OTTIMIZZARE O SEMPLIFICARE PARTI CONSOLIDATE.
  * IN CASO DI MODIFICHE NON INTERESSATE DAL TASK, COPIARE E INCOLLARE INTEGRALMENTE IL CODICE PRECEDENTE.
  */
@@ -7,5 +7,14 @@ import { Render } from './render.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     Render.initLayout();
-    console.log("Sistema Inizializzato - Versione 1.0.0");
+
+    // DATI DI TEST PER FORZARE LA COMPARSA DELLA SEZIONE
+    const testData = [
+        { id: '1', nome: 'ALP', immagine_url: 'https://i.imgur.com/kxXO0Oh.jpg' },
+        { id: '2', nome: 'BCM', immagine_url: 'https://i.imgur.com/PY1VsKj.jpg' }
+    ];
+    
+    Render.publishers(testData);
+
+    console.log("Sistema Inizializzato - Versione 1.1.1");
 });
