e0e3da225ea456a179faf9dc1ce31bf8af83c1bf
temperarcher <temper.archer@gmail.com>
2026-01-25 18:33:25 +0100

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,6 +1,7 @@
 /**
  * VERSION: 1.1.4
  * PROTOCOLLO DI INTEGRITÀ: È FATTO DIVIETO DI OTTIMIZZARE O SEMPLIFICARE PARTI CONSOLIDATE.
+ * IN CASO DI MODIFICHE NON INTERESSATE DAL TASK, COPIARE E INCOLLARE INTEGRALMENTE IL CODICE PRECEDENTE.
  */
 import { sections } from './ui/layout/sections.js';
 import { header } from './ui/layout/header.js';
