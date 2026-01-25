86c6ed9e87290d90ee1cc6825e7b066e7807c673
temperarcher <temper.archer@gmail.com>
2026-01-25 09:54:08 +0100

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -4,7 +4,7 @@
 import { header } from './ui/layout/header.js';
 import { publishers } from './ui/layout/publishers.js';
 import { series } from './ui/layout/series.js';
-import { sections } from './ui/layout/sections.js'; // Nuovo
+import { sections } from './ui/layout/sections.js';
 import { container } from './ui/grid/container.js';
 import { card } from './ui/grid/card.js';
 import { base } from './ui/modal/base.js';
@@ -13,7 +13,7 @@ import { form } from './ui/modal/form.js';
 
 export const UI = {
     HEADER: header,
-    ROOTS: sections, // Aggiunto
+    ROOTS: sections,
     PUBLISHER_SECTION: publishers.SECTION,
     PUBLISHER_PILL: publishers.PILL,
     ALL_PUBLISHERS_BUTTON: publishers.ALL_BUTTON,
