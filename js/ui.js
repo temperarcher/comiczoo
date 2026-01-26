6308fcdb85bf13e75c23acea4551c3b504b5372b
temperarcher <temper.archer@gmail.com>
2026-01-26 21:39:56 +0100

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,5 +1,5 @@
 /**
- * VERSION: 1.1.9
+ * VERSION: 1.2.0
  * PROTOCOLLO DI INTEGRITÀ: È FATTO DIVIETO DI OTTIMIZZARE O SEMPLIFICARE PARTI CONSOLIDATE.
  * IN CASO DI MODIFICHE NON INTERESSATE DAL TASK, COPIARE E INCOLLARE INTEGRALMENTE IL CODICE PRECEDENTE.
  */
@@ -8,6 +8,7 @@ import { header } from './ui/layout/header.js';
 import { publishers } from './ui/layout/publishers.js';
 import { series } from './ui/layout/series.js';
 import { issues } from './ui/layout/issues.js';
+import { modal } from './ui/layout/modal.js';
 
 export const UI = {
     ROOTS: sections,
@@ -17,7 +18,16 @@ export const UI = {
     ALL_PUBLISHERS_BUTTON: publishers.ALL_BUTTON,
     SERIES_SECTION: series.SECTION,
     SERIES_CARD: series.CARD,
-    // Registrazione atomi Issues per l'HUB
     ISSUES_SECTION: issues.SECTION,
-    ISSUES_CARD: issues.CARD
+    ISSUES_CARD: issues.CARD,
+    // HUB MODALE - Integrità mantenuta
+    MODAL_WRAPPER: modal.WRAPPER,
+    MODAL_LEFT_COL: modal.LEFT_COL,
+    MODAL_RIGHT_COL: modal.RIGHT_COL,
+    MODAL_STORY_ITEM: modal.STORY_ITEM,
+    MODAL_DETAIL_ROW: modal.DETAIL_ROW,
+    MODAL_CLOSE: (e) => {
+        if(e) e.stopPropagation();
+        window.closeModal();
+    }
 };
