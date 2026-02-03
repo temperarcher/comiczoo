005a882106284ccf9efa5fd2040c8043780db22b
temperarcher <temper.archer@gmail.com>
2026-02-03 19:56:56 +0100

    Update app.js

--- comiczoo/app.js
+++ comiczoo/app.js
@@ -1,24 +1,24 @@
 import { renderHeader } from './components/header.js';
 import { renderSearchEditor } from './components/search-editor.js';
 import { renderSeriesSelector } from './components/series-selector.js';
+import { renderGrid } from './components/grid.js';
 
 const app = document.getElementById('app');
 
 async function init() {
-    // Costruzione dinamica dello scheletro
     app.innerHTML = `
         <header id="header-container"></header>
         <section id="search-editor-container"></section>
         <section id="series-selector-container"></section>
-        <main id="grid-container" class="container mx-auto p-6"></main>
+        <main id="grid-container"></main>
     `;
 
-    // Montaggio Atomi
     renderHeader();
     await renderSearchEditor();
     await renderSeriesSelector();
+    await renderGrid(); // Inizialmente carica tutto
     
-    console.log('COMICZOO: Pipeline filtri completata.');
+    console.log('COMICZOO: Interfaccia completa caricata.');
 }
 
 init();
