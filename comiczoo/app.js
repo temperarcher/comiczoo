c10a01c8946c022743303658ffc2109a1c563407
temperarcher <temper.archer@gmail.com>
2026-02-08 20:52:55 +0100

    Update app.js

--- comiczoo/app.js
+++ comiczoo/app.js
@@ -3,11 +3,13 @@ import { renderSearchEditor } from './components/search-editor.js';
 import { renderSeriesSelector } from './components/series-selector.js';
 import { renderGrid } from './components/grid.js';
 import { openIssueModal } from './modals/issue-modal.js';
+// Importiamo il cervello dell'editing
+import { initEditSystem } from './core/edit-handler.js'; 
 
 const app = document.getElementById('app');
 
 async function init() {
-    // 1. Iniezione Atomica dello Scheletro (Tutto in una volta)
+    // 1. Iniezione Atomica dello Scheletro
     app.innerHTML = `
         <header id="header-container"></header>
         <section id="search-editor-container"></section>
@@ -17,13 +19,21 @@ async function init() {
     `;
 
     // 2. Registrazione Eventi Globali (Blindati)
+    
+    // Gestione apertura Modale
     window.addEventListener('comiczoo:open-modal', (e) => {
         openIssueModal(e.detail);
     });
 
+    // Inizializzazione Sistema di Editing (Hybrid)
+    initEditSystem();
+
     // 3. Montaggio componenti
+    // Carichiamo prima l'interfaccia statica
     renderHeader();
     await renderSearchEditor();
+    
+    // Poi i dati dinamici
     await renderSeriesSelector();
     await renderGrid();
 }
