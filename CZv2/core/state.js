400afa170145f113453ea42feed3181d54ad0ca2
temperarcher <temper.archer@gmail.com>
2026-02-16 21:09:16 +0100

    Update state.js

--- CZv2/core/state.js
+++ CZv2/core/state.js
@@ -1,17 +1,12 @@
 // CZv2/core/state.js
 export const AppState = {
     current: {
-        codice_editore_id: 'all',
+        codice_editore_id: 'all', // Non più editore_id
         serie_id: null,
-        issue_id: null,
         search_query: ''
     },
-    
     set(key, value) {
         this.current[key] = value;
-        // Logica di reset a cascata
         if (key === 'codice_editore_id') this.current.serie_id = null;
-        
-        console.log(`[State Update] ${key}:`, value);
     }
 };
