ffc7028a6530ce76f417a2b9efde32755d442cc7
temperarcher <temper.archer@gmail.com>
2026-02-16 19:21:53 +0100

    Create state.js

---
+++ CZv2/core/state.js
@@ -0,0 +1,8 @@
+// CZv2/core/events.js
+export const CZ_EVENTS = {
+    EDITORE_CHANGED: 'cz:editore-changed',   // Quando clicchi un Editore
+    SERIE_CHANGED: 'cz:serie-changed',       // Quando clicchi una Serie
+    DATA_UPDATED: 'cz:data-updated',         // Quando un record viene salvato
+    OPEN_MODAL: 'cz:open-modal',             // Quando apri il dettaglio
+    GRID_REFRESH: 'cz:grid-refresh'          // Quando la griglia deve aggiornarsi
+};
